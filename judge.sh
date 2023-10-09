9267d88e4bc407601e12232477bf27c2b1934765
Dillon Winchell <dwinchel@redhat.com>
2023-10-09 00:26:35 -0400

    Shell based judging script

--- judge.sh
+++ judge.sh
@@ -2,6 +2,8 @@
 
 # Achievement: I Will Comply
 for s in $(oc get compliancescan -n openshift-compliance -o name); do
-    if [ $(oc get $s -o yaml | yq '.status.result') == "COMPLIANT" ]; then echo Achievement Unlocked! - I Will Comply; fi
+    if [ $(oc get $s -o yaml | yq '.status.result') == "COMPLIANT" ]; then echo Achievement Unlocked! - I Will Comply; 
+	    else echo "No Achievements Earned"
+	    fi
 done
 
